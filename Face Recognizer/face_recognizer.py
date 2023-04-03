42b7d5ad5c47ba36888819592107802389cbaf70
rodrigoschardong <rscharong@gmail.com>
2023-04-03 14:04:17 -0300

    feat: Face Recognizer Trainning

--- Face Recognizer/face_recognizer.py
+++ Face Recognizer/face_recognizer.py
@@ -1,7 +1,15 @@
+"""
+    python -m pip install --user opencv-contrib-python
+"""
+
 from PIL import Image
 import cv2 
 import numpy as np
 from matplotlib import pyplot as plt 
+import os
+#import imageio.mimread
+import urllib.request
+
 
 
 def DisplayInOut(inImage, outImage, inIsGray = 0, outIsGray = 0):   
@@ -30,15 +38,69 @@ def DisplayImg(inImage, inIsGray = 0,):
     plt.title("Image")
     plt.plot()
 
+class ImageBank():
+    def __init__(self, paths):
+        self.len = len(paths)
+        self.imagesPaths = paths
+        self.ids = []
+        self.subjects = []
+        id = 0
+        for path in paths:
+            subject = os.path.split(path)[1].split('.')[0]
+            index = int(subject.replace('subject', ''))
+            self.ids.append(index)
+            self.subjects.append(subject)
+        #print(self.subjects)
+        #print(self.ids)
+
+    def GetImages(self):
+        images = []
+        for path in self.imagesPaths:
+            #img = cv2.imread(path, 0)
+            img = Image.open(path).convert('L')
+            
+            images.append(np.array(img, 'uint8'))
+
+        #print(images[0])
+        #cv2.imwrite("hey.png", images[0])
+        return np.array(images)
+
 class FaceRecognizer():
-    def __init__(self, image):
-        self.image = image.copy()
+    def __init__(self, classifierPath):
+
+        self.classifierPath = classifierPath
         self.proportion = [1,1]
-        self.lastImage = self.image.copy()
+        if(not(os.path.exists(classifierPath))):
+            os.mkdir(classifierPath)
+
+    def Train(self, trainPaths):
+        imgBank = ImageBank(trainPaths)
+        ids = np.array(imgBank.ids)
+        images = imgBank.GetImages()
+        lbph_classifier = cv2.face.LBPHFaceRecognizer_create()
+        
+        lbph_classifier.train(images, ids)
+        lbph_classifier.write(self.classifierPath + 'lbph_classifer.yml')
 
     def DisplayImage(self):
         DisplayImg(self.lastImage)
 
 
+def get_images_paths(path):
+    paths = [(path+ '/' + f) for f in os.listdir(path)]
+    #paths = os.listdir(path)
+    #print(paths)
+    return paths
+
+
+
 if __name__ == '__main__':
-    pass
+    print("Face Recognizer init")
+    trainPath = "./images/train"
+    testPath = "./images/test"
+    classifierPath = "./classifiers"
+
+    faceRec = FaceRecognizer(classifierPath)
+
+    #paths = get_images_paths(trainPath)
+    faceRec.Train(get_images_paths(trainPath))
